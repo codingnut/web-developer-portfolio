fbbc459532ae47756e9e2899c763cb8830a218ad
Shashi Kant <Skant743@gmail.com>
2020-02-20 17:18:55 +0530

    initial commit

---
+++ js/resume.js
@@ -0,0 +1,44 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  });
+
+  Typed.new("#writing-text", {
+    strings: [
+      "Full-Stack Web Developer.", "love everything about code.", "solve problems."
+    ],
+    // Optionally use an HTML element to grab strings from (must wrap each string in a <p>)
+    stringsElement: null,
+    typeSpeed: 0,
+    contentType: 'text',
+    callback: function() {
+      $("#writing-text").css({"color": "#fff", "background-color": "#f8b413"});
+    },
+   // loop: true,
+    preStringTyped: function() {},
+    onStringTyped: function() {}
+  });
+
+})(jQuery); // End of use strict
